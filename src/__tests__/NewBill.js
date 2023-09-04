6b61b49e6cadeb719f979cae883dcd4ab3ac3513
EJ <eric_j_fra@yahoo.fr>
2023-09-04 22:17:00 +0200

    changes:    - the field values are those expected    - reset package.json, OC clone

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -9,9 +9,7 @@ import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
 import { localStorageMock } from "../__mocks__/localStorage.js"
 import router from "../app/Router.js"
 import userEvent from '@testing-library/user-event';
-// import { mockStore } from '../__mocks__/test-store.js';
 import store from '../__mocks__/store.js';
-import { each } from 'jquery';
 
 
 const context = {
@@ -21,13 +19,13 @@ const context = {
   localStorage: window.localStorage,
 }
 const setContext = (ctx) => {
-  context.document = ctx.document | context.document
-  context.onNavigate = ctx.onNavigate | context.onNavigate
-  context.store = ctx.store | context.store
-  context.localStorage = ctx.localStorage | context.localStorage
+  context.document = ctx.document 
+  context.onNavigate = ctx.onNavigate 
+  context.store = ctx.store
+  context.localStorage = ctx.localStorage 
 }
 
-describe("Given I am connected as an employee, for vertical layout", () => {
+describe("Given I am connected as an employee", () => {
   beforeAll(()=> {
     Object.defineProperty(window, 'localStorage', { value: localStorageMock })
     window.localStorage.setItem('user', JSON.stringify({
@@ -41,7 +39,6 @@ describe("Given I am connected as an employee, for vertical layout", () => {
   })
 
   describe("When I am on NewBill Page", () => {
-
     test("Then newBill icon in vertical layout should be highlighted", async () => {
       await waitFor(() => screen.getByTestId('icon-mail'))
       const windowIcon = screen.getByTestId('icon-mail')
@@ -51,10 +48,9 @@ describe("Given I am connected as an employee, for vertical layout", () => {
   })
 
   describe("When I am on NewBill Page", () => {
-
     beforeEach(()=>{
       setContext({
-        document: document, // simule les interactions DOM
+        document, // simule les interactions DOM
         onNavigate: path => { document.body.innerHTML = ROUTES({ pathname:path, data: [] }) }, // simule la navigation en prenant un chemin "path" et modifie le contenu de la page
         store : null, // objet simulé, agit comme un magasin de données
         localStorage: window.localStorage,
@@ -71,7 +67,7 @@ describe("Given I am connected as an employee, for vertical layout", () => {
 
     test("handleChangeFile is call when the btn new bill is click", () => {
       const bill = new NewBill(context)
-      const btn = waitFor(()=>screen.getByTestId("file"))
+      const btn = screen.getByTestId("file")
       const handleChangeFileFn = jest.fn((e) => (bill.handleChangeFile(e)))
       btn.addEventListener('click', handleChangeFileFn)
       userEvent.click(btn)
@@ -84,7 +80,7 @@ describe("Given I am connected as an employee, for vertical layout", () => {
     test("Proof : test allowed upload type files : JPG, JPEG, PNG", () => {
       const bill = new NewBill(context)
       
-      const btn = waitFor(() => screen.getByTestId("file"))
+      const btn = screen.getByTestId("file")
       const wrongFile = new File(['toto'], 'toto.txt', { type: 'text/plain' })
       const handleChangeFileFn = jest.fn((e) => (bill.handleChangeFile(e)))
       btn.addEventListener('click', handleChangeFileFn)
@@ -110,80 +106,68 @@ describe("Given I am connected as an employee, for vertical layout", () => {
         localStorage: window.localStorage,
       })
 
-      instance = new NewBill(context)
       document.body.innerHTML = NewBillUI()
-
       // All the input in the newBill form
-      const expenseType = screen.getByTestId("expense-type")
-      const expenseName = screen.getByTestId("expense-name")
-      const expenseDatePicker = screen.getByTestId("datepicker")
+      const expenseType = await waitFor(() => screen.getByTestId("expense-type"))
+      const expenseName = await waitFor(() => screen.getByTestId("expense-name"))
+      const expenseDatePicker = await waitFor(() => screen.getByTestId("datepicker"))
       const expenseAmount = screen.getByTestId("amount")
       const expenseVat = screen.getByTestId("vat")
       const expensePct = screen.getByTestId("pct")
       const expenseCommentary = screen.getByTestId("commentary")
-      const expenseFile = screen.getByTestId("file")
-      
+      const expenseFile = await waitFor(() => screen.getByTestId("file"))
+      // const expenseFile = await waitFor(() => screen.getByText("Billed"))
 
-      /////////// MOCKED TEST STORE ///////////
-      // // mocked data from __mock__store.js through test-store.js
-      // let mockedName;
 
-      // mockStore().then((mockedBills) => {
-      //   mockedName = mockedBills[0].name
-      // })
-
-      /////////// MOCKED STORE  ///////////
       let mockedType;
       let mockedName;
       let mockedDatePicker;
       let mockedAmount;
       let mockedVat;
       let mockedPct;
-      let mockedCommentary = 'testValue'
-      let mockedFileName;
+      let mockedCommentary = 'commentary - testValue'
+      // let mockedFileName;
 
-      await store.bills().list()
+      await store.bills()
+      .list()
       .then(async (bills) => {
 
-        function mockData(data) {
-          if (typeof data.value !== 'string') {
-            data = data.toString()
-          }
-          return bills[0][data]
-        }
+        mockedType = await waitFor(() => bills[0].type)
+        mockedName = await waitFor(() => bills[0].name) 
+        mockedDatePicker = await waitFor(() => bills[0].date)
+        mockedAmount = await waitFor(() => bills[0].amount.toString())
+        mockedVat = await waitFor(() => bills[0].vat)
+        mockedPct = await waitFor(() => bills[0].pct.toString())
 
-        mockedType = await mockData('type')
-        mockedName = await mockData('name')
-        mockedDatePicker = await mockData('date')
-        mockedAmount = await mockData('amount')
-        mockedVat = await mockData('vat')
-        mockedPct = await mockData('pct')
-        // mockedFileName = await mockData('fileName')
-
-        // userEvent.selectOptions(expenseType, mockedType)
-        // userEvent.type(expenseName, mockedName)
-        // userEvent.type(expenseDatePicker, mockedDatePicker)
+        //for file(proof) simulate user upload with good extension (jpeg, jpg, png)
+        const mockedFile = await waitFor(() => bills[0].fileName)
+        // const lastIndex = mockedFile.lastIndexOf('.')
+        // const mockedFileNoExtension = lastIndex !== -1 ? mockedFile.slice(0, lastIndex) : mockedFile
+        // const mockedFileName = new File([mockedFileNoExtension],mockedFile , {type: 'image/jpg'} )
+        
+        userEvent.selectOptions(expenseType, mockedType)
+        userEvent.type(expenseName, mockedName) 
+        userEvent.upload(expenseDatePicker, mockedDatePicker)
         userEvent.type(expenseAmount, mockedAmount)
         userEvent.type(expenseVat, mockedVat)
         userEvent.type(expensePct, mockedPct)
         userEvent.type(expenseCommentary, mockedCommentary)
-        // userEvent.upload(expenseFile, mockedFileName)
-
-      
-        // expect(expenseType.value).toBe(mockedType)
-        // expect(expenseName.value).toBe(mockedName)
-        // expect(expenseDatePicker.value).toBe(mockedDatePicker)
+        userEvent.upload(expenseFile, mockedFile)
+        // userEvent.upload(expenseFile, mockedFileName.name)
+        
+        expect(expenseType.value).toBe(mockedType)
+        expect(expenseName.value).toBe(mockedName)
+        expect(expenseDatePicker.files[0]).toBe(mockedDatePicker)
         expect(expenseAmount.value).toBe(mockedAmount)
         expect(expenseVat.value).toBe(mockedVat)
         expect(expensePct.value).toBe(mockedPct)
         expect(expenseCommentary.value).toBe(mockedCommentary)
-        // expect(expenseFile.value).toStrictEqual(mockedFileName)
+        expect(expenseFile.files[0]).toBe(mockedFile)
+        // expect(expenseFile.files[0]).toBe(mockedFileName.name)
       })
-    
     }) 
   })
 })
-
 
 
 // tester POST handleSubmit
