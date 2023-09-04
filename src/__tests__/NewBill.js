4c6a83fcece9487e19b2a52f889be2b958b08330
EJ <eric_j_fra@yahoo.fr>
2023-09-04 14:56:34 +0200

    unfixed changes

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import '@testing-library/jest-dom/extend-expect'; // for toHaveAttribute()
+import '@testing-library/jest-dom/extend-expect'; // for matchers
 import { screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
@@ -11,96 +11,82 @@ import router from "../app/Router.js"
 import userEvent from '@testing-library/user-event';
 // import { mockStore } from '../__mocks__/test-store.js';
 import store from '../__mocks__/store.js';
+import { each } from 'jquery';
 
 
-let context;
-let instance;
+const context = {
+  document: document, // simule les interactions DOM
+  onNavigate: null,
+  store : null, // objet simulé, agit comme un magasin de données
+  localStorage: window.localStorage,
+}
+const setContext = (ctx) => {
+  context.document = ctx.document | context.document
+  context.onNavigate = ctx.onNavigate | context.onNavigate
+  context.store = ctx.store | context.store
+  context.localStorage = ctx.localStorage | context.localStorage
+}
 
-describe("Given I am connected as an employee", () => {
+describe("Given I am connected as an employee, for vertical layout", () => {
+  beforeAll(()=> {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'
+    }))
+    const root = document.createElement("div")
+    root.setAttribute("id", "root")
+    document.body.append(root)
+    router()
+    window.onNavigate(ROUTES_PATH.NewBill)
+  })
+
   describe("When I am on NewBill Page", () => {
 
-    //   context = {
-    //     document: document, // simule les interactions DOM
-    //     onNavigate: path => { document.body.innerHTML = ROUTES({ pathname:path, data: [] }) }, // simule la navigation en prenant un chemin "path" et modifie le contenu de la page
-    //     store : null, // objet simulé, agit comme un magasin de données
-    //     localStorage: window.localStorage,
-    //   }
-    //   beforeEach (()=>{
-    //     onNavigate = context.onNavigate
-    //     instance = new NewBill(context)
-    //     document.body.innerHTML = NewBillUI()
-    //     context.localStorage
-    //  })
-      // afterEach (()=> {
-      //  onNavigate = null
-      //  instance =  null 
-      //  document.body.innerHTML = null
-      //  context.localStorage.clear()
-    //    jest.restoreAllMocks()
-    //  })
-    
     test("Then newBill icon in vertical layout should be highlighted", async () => {
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.NewBill)
       await waitFor(() => screen.getByTestId('icon-mail'))
       const windowIcon = screen.getByTestId('icon-mail')
-
+      expect(windowIcon.classList).toBeTruthy()
       expect(windowIcon.classList.contains('active-icon')).toBe(true)
+    })
+  })
+
+  describe("When I am on NewBill Page", () => {
+
+    beforeEach(()=>{
+      setContext({
+        document: document, // simule les interactions DOM
+        onNavigate: path => { document.body.innerHTML = ROUTES({ pathname:path, data: [] }) }, // simule la navigation en prenant un chemin "path" et modifie le contenu de la page
+        store : null, // objet simulé, agit comme un magasin de données
+        localStorage: window.localStorage,
+      })
+      document.body.innerHTML = NewBillUI()
     })
 
     test("Then when le bouton -choisir un fichier- est bien de type file et required", () => {
       // const html = NewBillUI()
-      document.body.innerHTML = NewBillUI()
       const input = screen.getByTestId("file");
       expect(input).toHaveAttribute('type', 'file')
       expect(input.required).toBe(true)
     })
 
     test("handleChangeFile is call when the btn new bill is click", () => {
-      context = {
-        document: document, // simule les interactions DOM
-        onNavigate: path => { document.body.innerHTML = ROUTES({ pathname:path, data: [] }) }, // simule la navigation en prenant un chemin "path" et modifie le contenu de la page
-        store : null, // objet simulé, agit comme un magasin de données
-        localStorage: window.localStorage,
-      } 
-
-      onNavigate = context.onNavigate
-      instance = new NewBill(context)
-      document.body.innerHTML = NewBillUI()
-      context.localStorage
-
-      const btn =screen.getByTestId("file")
-      const handleChangeFileFn = jest.fn((e) => (instance.handleChangeFile(e)))
+      const bill = new NewBill(context)
+      const btn = waitFor(()=>screen.getByTestId("file"))
+      const handleChangeFileFn = jest.fn((e) => (bill.handleChangeFile(e)))
       btn.addEventListener('click', handleChangeFileFn)
       userEvent.click(btn)
-      expect(handleChangeFileFn).toHaveBeenCalledTimes(1)
+      expect(handleChangeFileFn).toHaveBeenCalled()
+      btn.removeEventListener('click', handleChangeFileFn )
     })
 
     // TODO wrong/good file
 
     test("Proof : test allowed upload type files : JPG, JPEG, PNG", () => {
-      context = {
-        document: document, // simule les interactions DOM
-        onNavigate: path => { document.body.innerHTML = ROUTES({ pathname:path, data: [] }) }, // simule la navigation en prenant un chemin "path" et modifie le contenu de la page
-        store : null, // objet simulé, agit comme un magasin de données
-        localStorage: window.localStorage,
-      } 
-
-      onNavigate = context.onNavigate
-      instance = new NewBill(context)
-      document.body.innerHTML = NewBillUI()
-      context.localStorage
-
-      const btn = screen.getByTestId("file")
+      const bill = new NewBill(context)
+      
+      const btn = waitFor(() => screen.getByTestId("file"))
       const wrongFile = new File(['toto'], 'toto.txt', { type: 'text/plain' })
-      const handleChangeFileFn = jest.fn((e) => (instance.handleChangeFile(e)))
+      const handleChangeFileFn = jest.fn((e) => (bill.handleChangeFile(e)))
       btn.addEventListener('click', handleChangeFileFn)
       userEvent.upload(btn, wrongFile)
       const errorDiv = screen.getByText("Extension de fichier non valide. Sélectionner un fichier JPG, JPEG ou PNG")
@@ -108,6 +94,7 @@ describe("Given I am connected as an employee", () => {
      
       expect(btn.files[0]).toStrictEqual(wrongFile)
       expect(errorDiv).toBeInTheDocument();
+      btn.removeEventListener('click', handleChangeFileFn )
       // expect ($('#file-error').css('display')).toBe('block')
       // expect(error.style.display).toBe('block')
     })
@@ -115,28 +102,27 @@ describe("Given I am connected as an employee", () => {
 
 
     test("Submit, function handleSubmit", async () => {
-      context = {
+      setContext ({
         document: document, // simule les interactions DOM
         onNavigate: path => { document.body.innerHTML = ROUTES({ pathname:path, data: [] }) }, // simule la navigation en prenant un chemin "path" et modifie le contenu de la page
         store : store, // objet simulé, agit comme un magasin de données
-        // store : null, // objet simulé, agit comme un magasin de données
+        // store : null,
         localStorage: window.localStorage,
-      } 
+      })
 
-      onNavigate = context.onNavigate
       instance = new NewBill(context)
       document.body.innerHTML = NewBillUI()
-      context.localStorage
 
       // All the input in the newBill form
       const expenseType = screen.getByTestId("expense-type")
       const expenseName = screen.getByTestId("expense-name")
-      const datepicker= screen.getByTestId("datepicker")
-      const amount = screen.getByTestId("amount")
-      const vat = screen.getByTestId("vat")
-      const pct = screen.getByTestId("pct")
-      const commentary = screen.getByTestId("commentary")
-      const file = screen.getByTestId("file")
+      const expenseDatePicker = screen.getByTestId("datepicker")
+      const expenseAmount = screen.getByTestId("amount")
+      const expenseVat = screen.getByTestId("vat")
+      const expensePct = screen.getByTestId("pct")
+      const expenseCommentary = screen.getByTestId("commentary")
+      const expenseFile = screen.getByTestId("file")
+      
 
       /////////// MOCKED TEST STORE ///////////
       // // mocked data from __mock__store.js through test-store.js
@@ -147,25 +133,60 @@ describe("Given I am connected as an employee", () => {
       // })
 
       /////////// MOCKED STORE  ///////////
+      let mockedType;
       let mockedName;
+      let mockedDatePicker;
+      let mockedAmount;
+      let mockedVat;
+      let mockedPct;
+      let mockedCommentary = 'testValue'
+      let mockedFileName;
 
-      await store.bills().list().then((bill) => {
-        mockedName = bill[0].name
-      })
+      await store.bills().list()
+      .then(async (bills) => {
+
+        function mockData(data) {
+          if (typeof data.value !== 'string') {
+            data = data.toString()
+          }
+          return bills[0][data]
+        }
+
+        mockedType = await mockData('type')
+        mockedName = await mockData('name')
+        mockedDatePicker = await mockData('date')
+        mockedAmount = await mockData('amount')
+        mockedVat = await mockData('vat')
+        mockedPct = await mockData('pct')
+        // mockedFileName = await mockData('fileName')
+
+        // userEvent.selectOptions(expenseType, mockedType)
+        // userEvent.type(expenseName, mockedName)
+        // userEvent.type(expenseDatePicker, mockedDatePicker)
+        userEvent.type(expenseAmount, mockedAmount)
+        userEvent.type(expenseVat, mockedVat)
+        userEvent.type(expensePct, mockedPct)
+        userEvent.type(expenseCommentary, mockedCommentary)
+        // userEvent.upload(expenseFile, mockedFileName)
+
       
-      userEvent.type(expenseName, mockedName)
+        // expect(expenseType.value).toBe(mockedType)
+        // expect(expenseName.value).toBe(mockedName)
+        // expect(expenseDatePicker.value).toBe(mockedDatePicker)
+        expect(expenseAmount.value).toBe(mockedAmount)
+        expect(expenseVat.value).toBe(mockedVat)
+        expect(expensePct.value).toBe(mockedPct)
+        expect(expenseCommentary.value).toBe(mockedCommentary)
+        // expect(expenseFile.value).toStrictEqual(mockedFileName)
+      })
     
-      expect(expenseName.value).toBe(mockedName)
-    })
+    }) 
   })
 })
 
 
 
-
-
-
 // tester POST handleSubmit
-// quand je clique sur envoyé je reviens sur la page bills
+// quand je clique sur envoyer je reviens sur la page bills
 // Uncovered lines 38-56,59-76,81-88
 // tester L'ID, status etc...???
